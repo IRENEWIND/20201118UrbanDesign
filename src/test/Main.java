a7b2552538ad196dd31fb348aa59cca711e5d798
IRENEWIND <2016972466@qq.com>
2020-11-18 20:10:05 +0800

    导入dxf

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,19 +1,34 @@
 package test;
 
 import processing.core.PApplet;
+import wblut.geom.WB_Point;
+import wblut.geom.WB_PolyLine;
+
+import java.awt.*;
+import java.util.ArrayList;
+
 
 public class Main extends PApplet {
     public static void main(String[] args) {
-        PApplet.main("test.main");
+        PApplet.main("test.Main");
     }
 
-    public void settings(){ size(400, 400); }
+    ArrayList<WB_PolyLine> polys = DXFImport.getDXFPolyLine("F:a.dxf","river");
 
-    public void setup(){
+    public void settings() {
+        size(400, 400);
+    }
+
+    public void setup() {
 
     }
 
-    public void draw(){
+    public void draw() {
 
     }
+
+
 }
+
+
+
