913bbd81b3c406e3a205b50cde75b51913c846a7
IRENEWIND <2016972466@qq.com>
2020-11-18 17:10:40 +0800

    随意加点东西

---
+++ src/test/Main.java
@@ -0,0 +1,19 @@
+package test;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+    public static void main(String[] args) {
+        PApplet.main("test.main");
+    }
+
+    public void settings(){ size(400, 400); }
+
+    public void setup(){
+
+    }
+
+    public void draw(){
+
+    }
+}
